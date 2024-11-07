efd3c9098e1469665bfa4f0340c19064c1fd8d6c
Amine-Bouguettaya <aminebouguettaya5@gmail.com>
2024-11-07 19:35:28 +0100

    ajout du reste des exercices

--- exo11.php
+++ exo11.php
@@ -1,4 +1,4 @@
-<h1>Exercice 10</h1>
+<h1>Exercice 11</h1>
 
 <p>Ecrire une fonction personnalisée qui affiche une date en français (en toutes lettres) à partir d’unechaîne de caractère représentant une date.</p>
 
@@ -7,12 +7,45 @@
 <?php
 
 
-echo formaterDateFr("2018-02-23");
+$jours = [
+    "Monday" => "Lundi",
+    "Tuesday" => "Mardi",
+    "Wednesday" => "Mercredi",
+    "Thursday" => "Jeudi",
+    "Friday" => "Vendredi",
+    "Saturday" => "Samedi",
+    "Sunday" => "Dimanche" 
+];
+
+$month = [
+    "January" => "Janvier",
+    "February" => "Février",
+    "March" => "Mars",
+    "April" => "Avril",
+    "May" => "Mai",
+    "June" => "Juin",
+    "July" => "Juillet",
+    "August" => "Aout",
+    "September" => "Septembre",
+    "October" => "Octobre",
+    "November" => "Novembre",
+    "December" => "Décembre" 
+];
+
+echo traduction_date(formaterDateFr("2018-02-23"), $jours, $month);
+
+function traduction_date($date, $jour, $mois) {
+    foreach ($jour as $jour_en => $jour_fr) {
+        $date = str_replace($jour_en, $jour_fr, $date);
+    }
+    foreach ($mois as $mois_en => $mois_fr) {
+        $date = str_replace($mois_en, $mois_fr, $date);
+    }
+    return $date;
+}
 
 function formaterDateFr($date) {
-    // $date = date_create($date);
-    // $result = date_format($date, 'l d F o');
-    setlocale(LC_TIME, "fr_FR");
-    $result = strftime("%A %d %B %G", strtotime($date));
+    $date = date_create($date);
+    $result = date_format($date, 'l d F o');
     return $result;
 }
---
+++ exo13.php
@@ -0,0 +1,12 @@
+<h1>Exercice 13</h1>
+
+<p>installation de xdebug et test</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+
+$array = [true,"texte",10,25.369,["valeur1","valeur2"]];
+
+var_dump($array);
